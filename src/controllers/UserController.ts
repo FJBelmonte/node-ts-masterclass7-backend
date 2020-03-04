dd0b60f0006ff8647e4bd147fe57cb87a6e46200
Fernando Belmonte <fernando.belmonte@live.com>
2020-03-04 19:47:02 -0300

    setup backend

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
     { name: 'Fernando', email: 'fernando@email.com' },
@@ -8,5 +9,20 @@ const users = [
 export default {
     async index(req: Request, res: Response) {
         return res.json(users);
+    },
+    async create(req: Request, res: Response) {
+        const emailService = new EmailService();
+        emailService.sendMail({
+            to: {
+                name: 'Fernando',
+                email: 'fernando@email.com'
+            },
+            message: {
+                content: '',
+                subject: 'Testando Typescript'
+            }
+        });
+
+        return res.send();
     }
 };
